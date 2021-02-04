73cc9b09ff6621d661ed1f104170eef7c350507d
drewp@bigasterisk.com <drewp@bigasterisk.com>
2021-02-03 23:07:25 -0800

    sometimes-working bake, but not reliable

--- world_export/run_scene_export.py
+++ world_export/run_scene_export.py
@@ -1,13 +1,20 @@
 import logging
 import os
 import sys
+import time
 from pathlib import Path
 
 import bpy
+import numpy
+from PIL import Image
 
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 log = logging.getLogger()
 build_out = Path("../build/client")
+
+
+def later(sec, func, *args, **kw):
+    bpy.app.timers.register(lambda: func(*args, **kw), first_interval=sec)
 
 
 def write_glb():
@@ -52,58 +59,83 @@ def write_glb():
     log.info("glb size %.1fKb" % (os.path.getsize(glb_out) / 1024))
 
 
-def bake(obj_name='gnd'):
-    log.info('bake start')
-    bpy.context.scene.cycles.use_denoising = True
-    bpy.context.scene.cycles.samples = 5
+class Bake:
+    bake_timeout = 10
+    def __init__(self, obj_name='gnd', cb=lambda: None):
+        self.obj_name = obj_name
+        self.cb = cb
+        # 'screen' is not immediately ready
+        later(1, self.cont2)
 
-    bpy.ops.object.select_all(action='DESELECT')
-    bpy.data.objects[obj_name].select_set(True)
-    bpy.context.view_layer.objects.active = bpy.data.objects[obj_name]
+    def cont2(self):
+        log.info('bake start')
+        bpy.context.scene.cycles.use_denoising = True
+        bpy.context.scene.cycles.samples = 10
 
-    mat = bpy.data.objects[obj_name].material_slots.values()[0].material
+        bpy.ops.object.select_all(action='DESELECT')
+        bpy.data.objects[self.obj_name].select_set(True)
+        bpy.context.view_layer.objects.active = bpy.data.objects[self.obj_name]
 
-    img = bpy.data.images.new('bake_out', 512, 512, alpha=False, is_data=True)
+        mat = bpy.data.objects[
+            self.obj_name].material_slots.values()[0].material
 
-    nodes = mat.node_tree.nodes
-    log.info(f'mat had {len(nodes)} nodes')
+        self.img = bpy.data.images.new('bake_out',
+                                       2048,
+                                       2048,
+                                       alpha=False,
+                                       is_data=True)
 
-    tx = nodes.new('ShaderNodeTexImage')
-    tx.image = img
+        nodes = mat.node_tree.nodes
+        log.info(f'mat had {len(nodes)} nodes')
 
-    tx.select = True  # bake writes to the selected node
-    nodes.active = tx
+        tx = nodes.new('ShaderNodeTexImage')
+        tx.image = self.img
 
+        tx.select = True  # bake writes to the selected node
+        nodes.active = tx
 
+        bpy.context.scene.render.engine = 'CYCLES'
+        bpy.context.scene.cycles.device = 'GPU'
 
-    bpy.app.timers.register(lambda: bake2(obj_name, img), first_interval=1)
-    # bake2(obj_name, img)
+        self.runs = [
+            ('COMBINED', 'comb', 'sRGB'),
+            ('DIFFUSE', 'dif', 'sRGB'),
+            ('AO', 'ao', 'Non-Color'),
+            ('SHADOW', 'shad', 'Non-Color'),
+            ('NORMAL', 'norm', 'Non-Color'),
+            ('ROUGHNESS', 'ruff', 'Non-Color'),
+            ('GLOSSY', 'glos', 'Non-Color'),
+        ]
 
+        self.nextBake()
 
-def bake2(obj_name, img):
-    bpy.context.scene.render.engine = 'CYCLES'
-    bpy.context.scene.cycles.device = 'GPU'
-    # bpy.context.space_data.shading.type = 'RENDERED'
-    # bpy.data.screens['Shading'].shading.type = 'RENDERED'
-    # bpy.ops.wm.redraw_timer(type='DRAW_WIN_SWAP', iterations=1)
-    bpy.ops.render.render()
+    def nextBake(self):
+        if not self.runs:
+            log.info('bake jobs done')
+            self.cb()
+            return
+        self.bakeAndSave()
 
-    for bake_type, out_name in [
-        #('COMBINED', 'combined'),
-        ('DIFFUSE', 'dif'),
-        ]:
+    def markImage(self):
+        self.img.pixels[0:3] = .1, .2, .3
+        self.sentinel = self.img.pixels[0:3]
+
+    def bakeAndSave(self):
+        # alternately, https://blender.stackexchange.com/questions/39716/running-code-after-a-modal-operator-finished-its-job
+        bake_type, out_name, cs = self.runs[0]
         log.info(f'start {bake_type} bake')
+        self.img.colorspace_settings == cs
+        self.markImage()
+
         bpy.context.scene.cycles.bake_type = bake_type
         bpy.ops.object.bake(
+            'INVOKE_DEFAULT',
             type=bake_type,
             # pass_filter={
             #     'COLOR', 'INDIRECT', 'EMIT', 'DIRECT', 'TRANSMISSION', 'AO',
             #     'GLOSSY', 'DIFFUSE'
             # },
-            # # filepath="",
-            # # width=512,
-            # # height=512,
-            # margin=16,
+            margin=16,
             # use_selected_to_active=False,
             # # max_ray_distance=0,
             # # cage_extrusion=0,
@@ -112,35 +144,72 @@ def bake2(obj_name, img):
             # # normal_r='POS_X',
             # # normal_g='POS_Y',
             # # normal_b='POS_Z',
-            # target='IMAGE_TEXTURES',
-            # # save_mode='INTERNAL',
-            # use_clear=True,
+            target='IMAGE_TEXTURES',
+            save_mode='INTERNAL',
+            use_clear=True,
             # # use_cage=False,
             # # use_split_materials=False,
             # # use_automatic_name=False,
             # # uv_layer=""
         )
-        image_save(img, build_out / f'bake_{obj_name}_{out_name}.png')
-        
-    log.info('bakes done')
+
+        def save():
+            image_save(self.img,
+                       build_out / f'bake_{self.obj_name}_{out_name}.png')
+            self.runs.pop(0)
+            # sometimes the next bake wouldn't start
+            later(1, self.nextBake)
+
+        log.info(f"waiting for bake image output for {bake_type}")
+        self.startPoll = time.time()
+        self.pollBakeDone(self.img, save)
+
+    def pollBakeDone(self, img, onChanged):
+        dt = time.time() - self.startPoll
+        log.info(f'poll image change, {dt:.1f} sec')
+        if dt > self.bake_timeout:
+            log.info("too slow- retrying")
+            self.bakeAndSave()
+            return
+        if img.pixels[0:3] != self.sentinel:
+            later(3, onChanged)
+            return
+        later(.5, self.pollBakeDone, img, onChanged)
 
 
-def image_save(img, path):
-    img.filepath = str(path)
+def image_save_builtin(img, path):
+    # having blender save was turning the image back to black!
+    img.filepath = str(path).replace('.png', '-builtinsave.png')
     img.file_format = "PNG"
     img.save()
-    log.info(f'wrote {img.filepath}')
+    log.info(f'wrote {path}')
+
+def image_save(img, path):
+    ar = numpy.array(img.pixels).reshape((img.size[0], img.size[1], 4))
+    ar = ar[::-1, :, :]
+    if img.colorspace_settings.name == 'sRGB':
+        ar = ar**(1 / 2.2)  # unverified
+    img = Image.fromarray((ar * 255).astype(numpy.uint8))
+    img.save(path)
+    log.info(f'wrote {path}')
     # then use https://github.com/BinomialLLC/basis_universal
     # then bjs loads with https://doc.babylonjs.com/advanced_topics/mutliPlatTextures#basis-file-format
 
-log.info("appendig")
 
-bpy.ops.wm.open_mainfile(filepath='../client/asset/wrap/wrap.blend')
+def main():
+    bpy.ops.wm.open_mainfile(filepath='../client/asset/wrap/wrap.blend')
 
-# bpy.app.timers.register(bake, first_interval=1)
-bake()
 
-#write_glb()
-# bpy.ops.wm.quit_blender()
+    objs = ['rock_arch']
+    def pump():
+        if not objs:
+            return
+        Bake(objs.pop(), pump)
+    pump()
+    #write_glb()
+    # bpy.ops.wm.quit_blender()
 
-log.info("EOF")
+    log.info("EOF")
+
+
+main()
