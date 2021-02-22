25bd908196e178872fca3378bdf7bdc958659018
drewp@bigasterisk.com <drewp@bigasterisk.com>
2021-02-22 09:42:19 -0800

    getOrCreateNick

--- client/index.ts
+++ client/index.ts
@@ -95,7 +95,29 @@ class Game {
   }
 }
 
+function getOrCreateNick(): string {
+  const url = new URL(window.location.href);
+  const qparams = url.searchParams;
+  if (!qparams.has("nick")) {
+    let pairs: string[] = [];
+    pairs = pairs.concat(["th", "ar", "he", "te", "an", "se", "in", "me", "er", "sa"]);
+    pairs = pairs.concat(["nd", "ne", "re", "wa", "ed", "ve", "es", "le", "ou", "no"]);
+    pairs = pairs.concat(["to", "ta", "ha", "al", "en", "de", "ea", "ot", "st", "so"]);
+
+    let nick = "";
+    for (let i = 2 + Math.random() * 2; i > 0; i--) {
+      nick += pairs[Math.floor(Math.random() * pairs.length)];
+    }
+
+    qparams.append("nick", nick);
+    window.location.replace(url.toString());
+  }
+  return qparams.get("nick")!;
+}
+
 async function go() {
+  const nick = getOrCreateNick();
+
   const status = new StatusLine();
   const net = new Net(status);
 
