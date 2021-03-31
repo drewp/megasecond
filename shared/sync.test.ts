91a2ae94312fcbadaea350fc13d7e1f61f72546f
drewp@bigasterisk.com <drewp@bigasterisk.com>
2021-03-31 00:44:47 -0700

    first test

---
+++ shared/sync.test.ts
@@ -0,0 +1,22 @@
+import "jest";
+import { Engine } from "@trixt0r/ecs";
+import { WorldState } from "./WorldRoom";
+import { TrackEcsEntities } from "./SyncEcsToColyseus";
+import { IdEntity } from "./IdEntity";
+
+describe("sync", () => {
+  describe("TrackEcsEntities", () => {
+    let world: Engine;
+    let roomState: WorldState;
+    beforeEach(() => {
+      world = new Engine();
+      roomState = new WorldState();
+      new TrackEcsEntities(world, roomState);
+    });
+    it("should add target entities to match source", () => {
+      expect(roomState.entities.size).toBe(0);
+      world.entities.add(new IdEntity());
+      expect(roomState.entities.size).toBe(1);
+    });
+  });
+});
