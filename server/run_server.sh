3cc7439afc1c4f21e990f19f7ba52b5509cce5a0
drewp@bigasterisk.com <drewp@bigasterisk.com>
2023-07-03 19:00:10 -0700

    refactor

--- server/run_server.sh
+++ server/run_server.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo Logs are being captured by frontail.
-pnpm run_server |& \
-  pnpx frontail \
+pnpm exec ts-node-dev --respawn --exit-child --debug -- server/index.ts |& \
+  pnpm exec frontail \
     --ui-highlight \
     --ui-highlight-preset server/log-highlight.json \
     --port 9002 \
     --url-path /log/server \
-    -
+    "-"
