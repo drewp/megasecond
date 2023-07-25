d627b829c6770c612d809e86658da052218dea05
drewp@bigasterisk.com <drewp@bigasterisk.com>
2023-07-24 17:11:00 -0700

    attempt to run blender gui from within docker

---
+++ world_export/blender_gui.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+exec docker run --gpus all -it -v $XSOCK:$XSOCK:rw -v $XAUTH:$XAUTH:rw \
+  --device=/dev/dri/card0:/dev/dri/card0 -e DISPLAY=$DISPLAY -e XAUTHORITY=$XAUTH mega_blender blender
